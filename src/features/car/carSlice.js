3062895132a80128d527c3e52f6327c182f4508e
Sebastian <sebastiandevelops@gmail.com>
2022-01-23 17:54:32 +0200

    Inital Commit

---
+++ src/features/car/carSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    cars : [ "Model S", "Model 3", "Model X", "Model Y" ]
+}
+
+const carSlice = createSlice({
+    name: "Car",
+    initialState,
+    reducers: {},
+})
+
+export const selectCars =state => state.cae.cars
+
+
+export default carSlice.reducer
